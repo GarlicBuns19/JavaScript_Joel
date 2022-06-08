e1adfd6d9f06abf7b83f7ebde29a1da7507573cb
GarlicBuns19 <planterx26@gmail.com>
2022-06-08 10:18:12 +0200

    option to if statement

--- Revision/code.js
+++ Revision/code.js
@@ -1,6 +1,16 @@
+// Option 1
 let age = 18;
 if(age >= 18){
     console.log('Well Done');
 }else{
     console.log('You\'re not welcome');
+}
+
+// Option 2
+if(age <= 18) {
+    console.log('You\'re are to young')
+}else if(age >= 18) {
+    console.log('You\'re getting old')
+}else {
+    console.log('I do\'nt know')
 }
