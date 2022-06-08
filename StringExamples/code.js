97b5d2fb96736b6a2a21892bccef4056f94cfbdd
GarlicBuns19 <planterx26@gmail.com>
2022-06-08 11:01:48 +0200

    $

--- StringExamples/code.js
+++ StringExamples/code.js
@@ -1,4 +1,12 @@
-let sentence = 'I love programming'
-let firstName;
+// Eg 1
+// let sentence = 'I love programming'
+// let firstName;
 
-console.log(sentence);
+// console.log(sentence);
+
+// Eg 2
+let firstName = 'Joel';
+let lastName = "Mukanya";
+
+console.log(firstName +' '+ lastName)
+console.log(`My fullname is ${firstName} ${lastName}`)
