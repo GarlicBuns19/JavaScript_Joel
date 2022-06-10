bc728cf31661439c3664ee1c75f7551eef21f312
GarlicBuns19 <planterx26@gmail.com>
2022-06-10 09:14:20 +0200

    help

--- Function example/code.js
+++ Function example/code.js
@@ -4,39 +4,38 @@ The default data type in JS is any,
 in case you haven't assigned a value.
 */
 // Variables
-let btnDisplay = document.querySelector('#display');
-let yourVar;
+// let btnDisplay = document.querySelector('#display');
+// let yourVar;
 // Example 1
-function display() {
-    document.getElementById('content').innerHTML = '<strong>Hello There</strong>';
-}
+// function display() {
+//     document.getElementById('content').innerHTML = '<strong>Hello There</strong>';
+// }
 // Example 2: Passing an argument or
 // parameter
-function displayWithArg(arg) {
-    document.getElementById('content').innerText = `Hello There ${arg}`;
-}
+// function displayWithArg(arg) {
+//     document.getElementById('content').innerText = `Hello There ${arg}`;
+// }
 // Example 3 -> default argument
-function addition(numb1, numb2 = 2) {
-    document.getElementById('content').innerText = `Sum is ${numb1 + numb2}`;
-}
+// function addition(numb1, numb2 = 2) {
+//     document.getElementById('content').innerText = `Sum is ${numb1 + numb2}`;
+// }
 // AddEventListeners to each buttons
-btnDisplay.addEventListener('click', display);
-//
+// btnDisplay.addEventListener('click', display);
 
-function multiplication(num1,num2){
-    return num1 + num2
-}
+// function multiplication(num1,num2){
+//     return num1 + num2
+// }
 
-console.log(multiplication(3,3))
+// console.log(multiplication(3,3))
 
-console.log(multiplication(3))
+// console.log(multiplication(3))
 // This gives u an error above
 
 
 // Another example
-let displayName = (firstName) => {
-    document.write('(<br>)(firstName)');
-}
+// let displayName = (firstName) => {
+//     document.write('<br> (firstName)');
+// }
 
 // displayName('Joel');
 // displayName('Your Name');
@@ -46,7 +45,7 @@ let displayName = (firstName) => {
 // }
 
 /* loops
-for = when you have a collection of data
+for = when you have a collection of data , let ,varables ,condition, intrement numbers,even number use if statement
 while = dont know how much loops you want to do
 do while
 for each
@@ -59,9 +58,14 @@ for in => object
 //     cnt++;
 // }
 
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-for (let q=0; q<numbers.length; q++) {
-    if (numbers[q] % 2 == 0) {
-        console.log(numbers[q]);
+let numbers = [1,2,3,4,5,6,7]
+for (let i=0; i<numbers.length; i++) {
+    // if (numbers[i] % 2 == 0) {
+    //     console.log(numbers[i]);
+    // }
+    console.log(numbers[i]);
+    if(numbers[i] == 6) {
+        break;
     }
 }
+// break, continue
