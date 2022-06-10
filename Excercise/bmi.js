3cc86e4e390550eb020334292ac3660ad025be41
GarlicBuns19 <planterx26@gmail.com>
2022-06-10 15:03:56 +0200

    done

--- Excercise/bmi.js
+++ Excercise/bmi.js
@@ -25,6 +25,44 @@ let age = document.querySelector('#age').value
         break
 
     };
+    let height = document.querySelector('#height').value
+    switch (true) {
+        case (height < 100):
+            document.getElementById('heightPar').innerText = ('You are growing');
+        break
+        case (height < 120):
+            document.getElementById('heightPar').innerText = ('You are in 100\'s');
+        break
+        case (height < 140):
+            document.getElementById('heightPar').innerText = ('You are almost 150');
+        break
+        case (height < 180):
+            document.getElementById('heightPar').innerText = ('You are a beast');
+        break
+        case (height < 200):
+            document.getElementById('heightPar').innerText = ('You are what you eating');
+        break
+
+    };
+    let weight = document.querySelector('#weight').value
+    switch (true) {
+        case (weight < 40):
+            document.getElementById('weightPar').innerText = ('You are light');
+        break
+        case (weight < 80):
+            document.getElementById('weightPar').innerText = ('You are getting fat');
+        break
+        case (weight < 140):
+            document.getElementById('weightPar').innerText = ('You are obest');
+        break
+        case (weight < 480):
+            document.getElementById('weightPar').innerText = ('You need help');
+        break
+        case (weight < 1000):
+            document.getElementById('weightPar').innerText = ('How are you alive');
+        break
+
+    };
 })
 
 
