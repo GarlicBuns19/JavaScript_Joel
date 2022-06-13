49ab939f1d0cf6d41eeae203049e63ad8547b427
GarlicBuns19 <planterx26@gmail.com>
2022-06-13 11:39:57 +0200

    help

--- API Excercise 1/script/code.js
+++ API Excercise 1/script/code.js
@@ -1,5 +1,3 @@
-
-
 async function getData() {
     let collections =
     await fetch('https://randomuser.me/api?results=3');
@@ -13,17 +11,18 @@ async function displayData() {
     let data = await getData();
     data.results.forEach(item => {
         wrapper += `
-        <div class="card" style="width: 18rem;">
-        <img src="${item['picture'].large}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${item['name'].first} ${item['name'].last}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>`
+                <div class="card" style="width: 18rem;">
+                    <img src="${item['picture'].large}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${item['name'].first} ${item['name'].last}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+        `
     });
-    let container = document.querySelector('.container');
-        container.innerHTML = wrapper;
+    let row = document.querySelector('.row');
+        row.innerHTML = wrapper;
 }
 
 //
