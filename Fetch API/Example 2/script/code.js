445c90578ad0b6b1fee489606d194efb6cbd588d
GarlicBuns19 <planterx26@gmail.com>
2022-06-13 09:35:54 +0200

    asynchronous function

---
+++ Fetch API/Example 2/script/code.js
@@ -0,0 +1,25 @@
+// Create an image
+let dadAndMe = document.createElement('img');
+//Place this element on the body
+document.body.appendChild(dadAndMe);
+/*
+Re-write example 1 by applying
+async and await
+*/
+
+// Create a function
+async function display() {
+    let res = await fetch('../images/acorn.png');
+    if(res.status == 200) {
+        let image = await res.blob();
+        dadAndMe.src = URL.createObjectURL(image);
+    }else{
+        console.error('Was not able to access your resource');
+    }
+}
+
+// Calling a function
+display().
+catch(e => {
+    console.error('PLease try again later');
+});
