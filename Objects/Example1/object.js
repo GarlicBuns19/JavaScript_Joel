55c6cb9ef7c990a2330ec80c1083e3ca77a34e96
GarlicBuns19 <planterx26@gmail.com>
2022-06-14 11:33:48 +0200

    help

--- Objects/Example1/object.js
+++ Objects/Example1/object.js
@@ -97,6 +97,7 @@
 // Dynamtic values of
 // The data does not change in ram
 // Creating boxes in ram,not changing the value,but use same umber,IMMUTABLE
+// Primitive values is IMMUTABLE,not changing on memory
 let data  = 32;
 console.log(data);
 console.log(typeof data);
