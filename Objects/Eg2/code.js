fb2fe9a65ca279fe2e394d74833b9e1db1ef8749
GarlicBuns19 <planterx26@gmail.com>
2022-06-14 13:18:01 +0200

    help

--- Objects/Eg2/code.js
+++ Objects/Eg2/code.js
@@ -1,14 +1,29 @@
 // structureClone()
-const num1 = {name: 'Hek'}
+// const num1 = {name: 'Hek'}
 
-const num2 = structuredClone(num1)
+// const num2 = structuredClone(num1)
 
-console.log(num1);
-console.log(num2);
-console.log(num1 !== num2);
+// console.log(num1);
+// console.log(num2);
+// console.log(num1 !== num2);
 
-var eg1 = {'name': 'Peter', 'count': 65};
-var eg2 = JSON.parse( JSON.stringify(eg1));
+// var eg1 = {'name': 'Peter', 'count': 65};
+// var eg2 = JSON.parse( JSON.stringify(eg1));
 
-console.log(eg2.name);
-console.log(eg2.count);
+// console.log(eg2.name);
+// console.log(eg2.count);
+
+let person = {name : 'Peter',surname: 'Cake',age: 40}
+let p1 = structuredClone(person);
+console.log(person);
+console.log(p1);
+
+// JSON.parse
+let person1 = {'name': 'Peter','surname': 'Cake','count': 40}
+let p2 = JSON.parse(JSON.stringify(person1));
+console.log(p2.name);
+console.log(p2.surname);
+console.log(p2.count);
+document.write(p2.name);
+document.write(p2.surname);
+document.write(p2.count);
