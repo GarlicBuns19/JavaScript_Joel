864923ece81a9337d6518a5dbc4ed794e0082bd8
GarlicBuns19 <planterx26@gmail.com>
2022-06-14 19:38:28 +0100

    done i hope

--- Loop/Example 1/code.js
+++ Loop/Example 1/code.js
@@ -1,12 +1,21 @@
+// 1
 for(let i = 1;i <= 10;i++){
     console.log(i);
 }
 
+// 2
 let arr = [7, 4, 3, 9,10, 12];
 console.log(arr);
 
+// 3
 const arr2 = [1,3,5,6,7,9,11];
 function deleteAnElement(n) {
     return console.log(n.splice(0,2))
 }
 deleteAnElement(arr2);
+
+// 4
+var s = ''
+for(q = 1;q <=8;q++){
+    console.log(s += q)
+}
