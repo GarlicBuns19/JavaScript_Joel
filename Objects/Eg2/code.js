0cf85d2d1ba96804d262605d86715a2ed5b67c12
GarlicBuns19 <planterx26@gmail.com>
2022-06-14 12:56:42 +0200

    help

--- Objects/Eg2/code.js
+++ Objects/Eg2/code.js
@@ -6,3 +6,9 @@ const num2 = structuredClone(num1)
 console.log(num1);
 console.log(num2);
 console.log(num1 !== num2);
+
+var eg1 = {'name': 'Peter', 'count': 65};
+var eg2 = JSON.parse( JSON.stringify(eg1));
+
+console.log(eg2.name);
+console.log(eg2.count);
